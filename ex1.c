7814fffd3d27e63d86cae301b0bfc2323d01ec61
Matheus Chediac Rodrigues <142456473+chediac@users.noreply.github.com>
2024-09-09 20:43:45 -0300

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+    pid_t pid_filho;
+
+    pid_filho = fork();
+
+    if (pid_filho < 0) {
+        printf("Erro ao criar processo filho!\n");
+        return 1;
+    } else if (pid_filho == 0) {
+        printf("Este é o processo filho. PID do filho: %d\n", getpid());
+        fflush(stdout);
+
+        for (int contador = 0; contador < 5; contador++) {
+            printf("Filho: Mensagem %d\n", contador + 1);
+            fflush(stdout);
+            sleep(1);
+        }
+    } else {
+        printf("Este é o processo pai. PID do pai: %d, PID do filho: %d\n", getpid(), pid_filho);
+        fflush(stdout);
+    }
+
+    return 0;
+}
